8215ebf77a61a3cb0865088319bc7c3392244ac7
Megaf <mmegaf@gmail.com>
2014-05-31 15:22:06 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -24,3 +24,19 @@ minetest.register_node("fire:flame", {
 	buildable_to = false,
 	damage_per_second = 1,
 })
+
+minetest.register_node("fire:basic_flame", {
+	description = "Non spreading Flame.",
+	inventory_image = "inv_flame.png",
+	drawtype = "plantlike",
+	tiles = {{
+		name="flame.png",
+		animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=1},
+	}},
+	light_source = 14,
+	groups = {dig_immediate=3,hot=3},
+	drop = 'default:coal_lump',
+	walkable = false,
+	buildable_to = false,
+	damage_per_second = 1,
+})
